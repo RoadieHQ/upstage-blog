ebc0892ed246302820c3ec5042aab84a7842f00a
David Tuite <dtuite@gmail.com>
2020-05-26 15:28:38 +0100

    Remove manifest and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
+const BLOG_NAME = `Larder Blog`;
+
 module.exports = {
   siteMetadata: {
-    title: `Larder Blog`,
+    title: BLOG_NAME,
     author: {
       name: `David Tuite`,
       summary: `builder of things for developers`,
@@ -62,18 +64,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
@@ -81,9 +71,6 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-jss`,
   ],
 };
